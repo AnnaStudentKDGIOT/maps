6c6c5e93522f6fa37a53c33aa8b07c43c2afc5f7
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-29 18:45:44 +0200

    kaart update

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,31 +1,24 @@
-var map = L.map('map').setView([32.164972,35.364135], 8);
+// initialize map once, at the zoom you want
+var map = L.map('map').setView([32.164972, 35.364135], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// use the osm-intl tiles with English labels
+L.tileLayer(
+  'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=en', {
+    maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+  }
+).addTo(map);
 
-L.marker([32.164972,35.364135]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+// add your overlays as before
+L.marker([32.164972, 35.364135])
+  .addTo(map)
+  .bindPopup('<b>Hello world!</b><br>I am a Leaflet marker.')
+  .openPopup();
 
-var map = L.map('map').setView([32.164972,35.364135], 13);
-
-// Add OpenStreetMap tiles as the base layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Add a marker at the same coordinates
-var marker = L.marker([32.164972,35.364135]).addTo(map);
-marker.bindPopup('<b>Hello world!</b><br>I am a Leaflet marker.').openPopup();
-
-// Add a circle overlay
-var circle = L.circle([32.164972,35.364135], {
+L.circle([32.164972, 35.364135], {
   color: 'red',
   fillColor: '#f03',
   fillOpacity: 0.5,
   radius: 500
-}).addTo(map);
-circle.bindPopup('I am a circle.');
-
-
+}).addTo(map)
+  .bindPopup('I am a circle.');
