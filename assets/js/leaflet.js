111ec89815052102cec32c32845c91c38c813400
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-29 18:38:32 +0200

    kaart update

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,14 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([32.164972,35.364135], 8);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([32.164972,35.364135]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([32.164972,35.364135], 13);
 
 // Add OpenStreetMap tiles as the base layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,11 +16,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker at the same coordinates
-var marker = L.marker([51.505, -0.09]).addTo(map);
+var marker = L.marker([32.164972,35.364135]).addTo(map);
 marker.bindPopup('<b>Hello world!</b><br>I am a Leaflet marker.').openPopup();
 
 // Add a circle overlay
-var circle = L.circle([51.508, -0.11], {
+var circle = L.circle([32.164972,35.364135], {
   color: 'red',
   fillColor: '#f03',
   fillOpacity: 0.5,
@@ -28,10 +28,4 @@ var circle = L.circle([51.508, -0.11], {
 }).addTo(map);
 circle.bindPopup('I am a circle.');
 
-// Add a polygon overlay
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(map);
-polygon.bindPopup('I am a polygon.');
+
